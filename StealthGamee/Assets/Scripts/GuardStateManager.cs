8afc8ae7abb6dcb186835b02b7280a6a355f6182
leongershko <68052617+leongershko@users.noreply.github.com>
2023-05-19 19:44:58 +0300

    fixed Guard Animations
    fix

--- StealthGamee/Assets/Scripts/GuardStateManager.cs
+++ StealthGamee/Assets/Scripts/GuardStateManager.cs
@@ -86,13 +86,14 @@ public class GuardStateManager : MonoBehaviour
             //Todo Remove Yellow Color
             //Debug.DrawRay(transform.position, transform.forward * viewingFieldDistance);
             //transform.Rotate(0, 1f, 0);
+
+
             //helping to wait for the guard to enter chase mode - animator skips chase mode without it.
             if (flagOfDelay)
             {
                 if (timerDelay >= 0)
                 {
                     timerDelay -= Time.deltaTime;
-                    //Debug.Log(timerDelay);
                 }
                 else
                 {
@@ -108,11 +109,10 @@ public class GuardStateManager : MonoBehaviour
                     animator.SetBool("visible", true);
                     flagOfDelay = true;
                     notVisibleTimer = NOT_VISIBLE_TIME;
-                    //workTimer = WORK_TIME;
                 }
                 else
                 {
-                    //workTimer -= Time.deltaTime;
+                    
                     //after 10 seconds of chasing without the target in sight stops chasing. and back to patrol
                     if (notVisibleTimer < 0)
                     {
